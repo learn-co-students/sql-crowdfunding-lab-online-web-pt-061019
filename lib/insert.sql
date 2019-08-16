07a72da098a9c2d582a980e676a4e5f34e641c85
Brittany Javalera <brittjavalera@gmail.com>
2019-08-16 01:53:05 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,129 @@
+INSERT INTO projects VALUES (1, "Help Me Buy a Taco Stand",
+"charity", 15000, "08/15/2019", "09/15/2019");
+
+INSERT INTO projects VALUES (2, "Help Pay My Medical Bills",
+"charity", 10000, "09/02/2019", "10/02/2019");
+
+INSERT INTO projects VALUES (3, "Urban Coyote Rescue",
+"charity", 35000, "08/16/2019", "11/16/2019");
+
+INSERT INTO projects VALUES (4, "Another Terence Malick Movie",
+"movies", 100000, "08/01/2019", "12/01/2019");
+
+INSERT INTO projects VALUES (5, "Custom Reptile Tank",
+"animals", 75000, "08/15/2019", "09/15/2019");
+
+INSERT INTO projects VALUES (6, "Shopping Carts with Dog Compartment ", "animals", 50000, "07/01/2019",
+"09/01/2019");
+
+INSERT INTO projects VALUES (7, "Lightweight Sunscreen",
+"cosmetics", 40000, "08/15/2019", "09/30/2019");
+
+INSERT INTO projects VALUES (8, "Private Rocket to Triton",
+"space", 1000000, "01/15/2019", "01/15/2020");
+
+INSERT INTO projects VALUES (9, "Star Wars Fan Fiction",
+"books", 30000, "08/01/2019", "09/01/2019");
+
+INSERT INTO projects VALUES (10, "Reshoot GOT Final Season",
+"television", 1000000, "07/15/2019", "08/31/2019");
+
+INSERT INTO users VALUES (1, "Rocky", 58);
+
+INSERT INTO users VALUES (2, "Janis", 27);
+
+INSERT INTO users VALUES (3, "Jimi", 27);
+
+INSERT INTO users VALUES (4, "Morrison", 27);
+
+INSERT INTO users VALUES (5, "Chuck", 50);
+
+INSERT INTO users VALUES (6, "Aretha", 33);
+
+INSERT INTO users VALUES (7, "Sharon", 25);
+
+INSERT INTO users VALUES (8, "Solange", 29);
+
+INSERT INTO users VALUES (9, "Bo", 22);
+
+INSERT INTO users VALUES (10, "Kat", 24);
+
+INSERT INTO users VALUES (11, "Maria", 45);
+
+INSERT INTO users VALUES (12, "James", 60);
+
+INSERT INTO users VALUES (13, "Lola", 35);
+
+INSERT INTO users VALUES (14, "Britt", 29);
+
+INSERT INTO users VALUES (15, "Tyler", 58);
+
+INSERT INTO users VALUES (16, "George", 42);
+
+INSERT INTO users VALUES (17, "Veronica", 33);
+
+INSERT INTO users VALUES (18, "Logan", 34);
+
+INSERT INTO users VALUES (19, "Eli", 34);
+
+INSERT INTO users VALUES (20, "Jaymes", 23);
+
+INSERT INTO pledges VALUES (01, 175, 17, 10);
+
+INSERT INTO pledges VALUES (02, 5000, 13, 1);
+
+INSERT INTO pledges VALUES (03, 60, 20, 2);
+
+INSERT INTO pledges VALUES (04, 10000, 10, 3);
+
+INSERT INTO pledges VALUES (05, 300, 14, 4);
+
+INSERT INTO pledges VALUES (06, 1500, 15, 5);
+
+INSERT INTO pledges VALUES (07, 2500, 12, 6);
+
+INSERT INTO pledges VALUES (08, 80, 9, 7);
+
+INSERT INTO pledges VALUES (09, 40, 6, 8);
+
+INSERT INTO pledges VALUES (10, 75, 3, 9);
+
+INSERT INTO pledges VALUES (11, 50, 5, 10);
+
+INSERT INTO pledges VALUES (12, 80, 7, 9);
+
+INSERT INTO pledges VALUES (13, 60, 9, 8);
+
+INSERT INTO pledges VALUES (14, 20, 11, 7);
+
+INSERT INTO pledges VALUES (15, 100, 13, 6);
+
+INSERT INTO pledges VALUES (16, 50, 15, 5);
+
+INSERT INTO pledges VALUES (17, 250, 16, 4);
+
+INSERT INTO pledges VALUES (18, 40, 17, 3);
+
+INSERT INTO pledges VALUES (19, 30, 19, 2);
+
+INSERT INTO pledges VALUES (20, 75, 20, 1);
+
+INSERT INTO pledges VALUES (21, 100, 18, 9);
+
+INSERT INTO pledges VALUES (22, 25, 16, 7);
+
+INSERT INTO pledges VALUES (23, 50, 14, 5);
+
+INSERT INTO pledges VALUES (24, 250, 12, 3);
+
+INSERT INTO pledges VALUES (25, 200, 10, 1);
+
+INSERT INTO pledges VALUES (26, 1000, 8, 10);
+
+INSERT INTO pledges VALUES (27, 50, 6, 8);
+
+INSERT INTO pledges VALUES (28, 75, 4, 6);
+
+INSERT INTO pledges VALUES (29, 20, 2, 4);
+
+INSERT INTO pledges VALUES (30, 500, 1, 2);
