c86be2b0b58e044fa2b62fd762d7998ef2b9677b
Sam Markham <sam.e.markham@gmail.com>
2019-08-20 14:35:32 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+--users
+INSERT INTO users VALUES (1, "Lynx", 17);
+INSERT INTO users VALUES (2, "Briony", 19);
+INSERT INTO users VALUES (3, "Wes", 19);
+INSERT INTO users VALUES (4, "Neha", 28);
+INSERT INTO users VALUES (5, "Mel", 28);
+INSERT INTO users VALUES (6, "Sam", 27);
+INSERT INTO users VALUES (7, "Nora", 17);
+INSERT INTO users VALUES (8, "Richard", 25);
+INSERT INTO users VALUES (9, "Julie", 60);
+INSERT INTO users VALUES (10, "Tom", 65);
+INSERT INTO users VALUES (11, "Kathy", 29);
+INSERT INTO users VALUES (12, "Daniela", 31);
+INSERT INTO users VALUES (13, "Tatiana", 17);
+INSERT INTO users VALUES (14, "Kelvin", 20);
+INSERT INTO users VALUES (15, "Nicola", 31);
+INSERT INTO users VALUES (16, "Brianna", 28);
+INSERT INTO users VALUES (17, "Shed", 31);
+INSERT INTO users VALUES (18, "Grace", 27);
+INSERT INTO users VALUES (19, "Eli", 4);
+INSERT INTO users VALUES (20, "Morris", 75);
+
+--projects
+INSERT INTO projects VALUES (1, "Dragon Dice", "Games", 3000, "March 6, 2019", "September 6, 2019");
+INSERT INTO projects VALUES (2, "Poetry Chapbook", "Books", 5000, "August 18, 2019", "September 18, 2019");
+INSERT INTO projects VALUES (3, "Awesome Board Game", "Games", 10000, "June 28, 2019", "August 15, 2019");
+INSERT INTO projects VALUES (4, "Fancy Drum Kit", "Music", 20000, "September 6, 2019", "October 6, 2019");
+INSERT INTO projects VALUES (5, "Laptop Charger", "Design & Tech", 15000, "August 20, 2019", "September 20, 2019");
+INSERT INTO projects VALUES (6, "Queer Zine", "Books", 8000, "August 17, 2019", "October 2, 2019");
+INSERT INTO projects VALUES (7, "Prosthetic Fingertips", "Design & Tech", 40000, "March 10, 2019", "September 20, 2019");
+INSERT INTO projects VALUES (8, "Retro Haven EP", "Music", 8800, "July 8, 2019", "August 28, 2019");
+INSERT INTO projects VALUES (9, "Queer Comic", "Books", 4500, "June 10, 2019", "July 10, 2019");
+INSERT INTO projects VALUES (10, "Unicorn Dice", "Games", 3000, "April 6, 2019", "October 6, 2019");
+
+--pledges
+INSERT INTO pledges VALUES (1, 1000, 1, 4);
+INSERT INTO pledges VALUES (2, 2000, 2, 1);
+INSERT INTO pledges VALUES (3, 500, 3, 5);
+INSERT INTO pledges VALUES (4, 2000, 4, 10);
+INSERT INTO pledges VALUES (5, 70, 4, 1);
+INSERT INTO pledges VALUES (6, 100, 5, 2);
+INSERT INTO pledges VALUES (7, 200, 6, 9);
+INSERT INTO pledges VALUES (8, 150, 7, 8);
+INSERT INTO pledges VALUES (9, 2000, 8, 6);
+INSERT INTO pledges VALUES (10, 100, 9, 5);
+INSERT INTO pledges VALUES (11, 200, 10, 7);
+INSERT INTO pledges VALUES (12, 30, 11, 8);
+INSERT INTO pledges VALUES (13, 40, 12, 3);
+INSERT INTO pledges VALUES (14, 500, 13, 2);
+INSERT INTO pledges VALUES (15, 2000, 14, 1);
+INSERT INTO pledges VALUES (16, 3000, 15, 2);
+INSERT INTO pledges VALUES (17, 4650, 16, 3);
+INSERT INTO pledges VALUES (18, 10, 17, 4);
+INSERT INTO pledges VALUES (19, 200, 18, 5);
+INSERT INTO pledges VALUES (20, 300, 19, 6);
+INSERT INTO pledges VALUES (21, 40, 20, 7);
+INSERT INTO pledges VALUES (22, 70, 20, 8);
+INSERT INTO pledges VALUES (23, 900, 19, 9);
+INSERT INTO pledges VALUES (24, 80, 18, 10);
+INSERT INTO pledges VALUES (25, 980, 17, 2);
+INSERT INTO pledges VALUES (26, 450, 16, 3);
+INSERT INTO pledges VALUES (27, 350, 15, 1);
+INSERT INTO pledges VALUES (28, 70, 14, 5);
+INSERT INTO pledges VALUES (29, 5, 13, 6);
+INSERT INTO pledges VALUES (30, 77, 12, 7);
