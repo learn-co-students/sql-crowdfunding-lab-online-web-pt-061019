7d7d470f8a323077683e0a740f47a50b458eb946
Nathan Rathe <nprathe10@gmail.com>
2019-08-20 00:44:20 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+title TEXT,
+catagory TEXT,
+funding_goal INTEGER,
+start_date TEXT,
+end_date INTEGER
+);
+
+CREATE TABLE users (
+name TEXT,
+age INTEGER
+);
+
+CREATE TABLE pledges (
+amount INTEGER
+);
